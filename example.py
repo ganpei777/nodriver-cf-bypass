f667f1922421d96037d2809db70ef0848cc48aa2
Ga18122 <86094702+ganpei777@users.noreply.github.com>
2025-05-30 18:57:29 +0800

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from nodriver_cf_bypass import CFBypass
 
 async def main() -> None:
     browser: nodriver.Browser = await nodriver.start()
-    browser_tab: nodriver.Tab = await browser.get("https://2captcha.com/demo/cloudflare-turnstile-challenge")
+    browser_tab: nodriver.Tab = await browser.get("https://www.nowsecure.nl/")
 
     CFB: CFBypass = CFBypass(_browser_tab = browser_tab, _debug = True)
     result = await CFB.bypass(_max_retries = 10, _interval_between_retries = 1, _reload_page_after_n_retries = 0)
